40338e1d3530be070638cd616941221d8ca9ffb5
Hector Lozano <hectorlg87@gmail.com>
2025-01-25 00:50:13 +0100

    objeto ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+
+const usuario = {
+    userName: 'Hector',
+    userLastName: 'Lozano',
+    temary: [
+        {subject: 'NodeJS', date: '2025-03-10'},
+        {subject: 'Git', date: '2025-02-02'},
+        {subject: 'React', date: '2025-05-12'}],
+    searchActive: true,
+}
+
+const viewDateReact = alumni.temary[2].date
+console.log(viewDateReact);
