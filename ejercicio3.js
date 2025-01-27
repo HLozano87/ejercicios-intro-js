6b937af16801d37780f3b745b722b7286343af67
Hector Lozano <hectorlg87@gmail.com>
2025-01-27 23:40:29 +0100

    fix: Se corrigen los return en las funciones.

--- ejercicio3.js
+++ ejercicio3.js
@@ -8,18 +8,18 @@ salvo el último elemento que sería la extensión del archivo que debe ir con u
 
 // Ejercicio 3.1
 
+const createFullPath = (list) => {
+    const path = list.slice(0, -1).join('/')
+    const extension = list[list.length -1]
+    return `${path}.${extension}`;
+};
+
 const input1 = [
     'Downloads',
     'Videos',
     'capture',
     'mp4',
 ]
-// create your function here
-const createFullPath = (list) => {
-    const path = list.slice(0, -1).join('/')
-    const extension = list[list.length -1]
-    return console.log(`${path}.${extension}`);
-};
 
 createFullPath(input1); // 'Downloads/Videos/capture.mp4'
 
@@ -46,29 +46,27 @@ createFullPath(input3) // 'programming/languages/easy/beginner/useful/pythonstuf
 
 // Ejercicio 3.2
 
-const input = 10;
-
-// create your function here
 const toStringAndSeparate = (entry) => {
     let result = entry.toString().split('').join('-')
-    return console.log(result);
+    return result;
 }
-    
-toStringAndSeparate(input)
+
+const input = 10;
+toStringAndSeparate(input) // '1-0'
 const secondInput = 1;
 toStringAndSeparate(secondInput); // '1'
 const thirdInput = 11234;
 toStringAndSeparate(thirdInput); // '1-1-2-3-4'
 
+
 // Ejercicio 3.3
 
-const toReverseModeAndLength = (word = String) => {
+const toReverseModeAndLength = (word) => {
     let reverseWord = word.split('').reverse().join('')
-    return console.log(`${reverseWord.length} ${reverseWord}`);
+    return `${reverseWord.length} ${reverseWord}`;
 }
 
 const inputStr1 = 'string';
-// create your function here
 toReverseModeAndLength(inputStr1); // '6 gnirts'
 const inputStr2 = 'variable';
 toReverseModeAndLength(inputStr2); // '8 elbairav'
