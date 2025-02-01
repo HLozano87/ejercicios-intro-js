5bb3503135b943da11329ebc18ac5262f0bf6bbc
Hector Lozano <hectorlg87@gmail.com>
2025-02-01 20:37:55 +0100

    fix: Solucion del bug de asincronia usando Promise

--- bugAsync.js
+++ bugAsync.js
@@ -1,13 +1,20 @@
 // Este programa simula una llamada asincrónica para obtener un usuario
 function obtenerUsuario(id) {
-    let usuario;
-    setTimeout(() => {
-        if (id === 1) {
-            usuario = { id: 1, nombre: 'John Doe' };
-        }
-    }, 2000);
-    return usuario;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!id || typeof id !== 'number') {
+                throw new Error('Debe introducir un número.')
+            }
+            if (id === 1) {
+                let usuario = { id: 1, nombre: 'John Doe' };
+                resolve(usuario)
+            }
+            reject(`El id "${id}", no está asociado a ningún usuario.`)
+        }, 2000);
+    })
+    
 }
-const usuario = obtenerUsuario(1);
-console.log(usuario);
 
+obtenerUsuario(3)
+.then(usuario => console.log(usuario))
+.catch(error => console.error(`¡¡Error durante la busqueda!!\n ${error}`))
