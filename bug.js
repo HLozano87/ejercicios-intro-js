3083d30cb1997bf033b81c0a34e6835ad2496f80
Hector Lozano <hectorlg87@gmail.com>
2025-03-13 13:08:55 +0100

    fix: se corrige error de calculo de promedio

--- bug.js
+++ bug.js
@@ -1,11 +1,10 @@
 const calcularPromedio = (numeros) => {
     let sumaTotal = 0;
-    for (let i = 0; i <= numeros.length; i++) {
-        sumaTotal += i;
+    if (numeros.length === 0) return 0
+    for (let i = 0; i < numeros.length; i++) {
+        sumaTotal += numeros[i];
     }
-    const promedio = sumaTotal / numeros.length;
-    return promedio;
+    return sumaTotal / numeros.length
 };
 const listaNumeros = [1, 2, 3, 4, 5];
-const promedioNumeros = calcularPromedio(listaNumeros);
-console.log(promedioNumeros);
+console.log(calcularPromedio(listaNumeros))
